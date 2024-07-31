a3b4d17878984df2b322246c0821b13fb99f7782
Jon Hunt <jonathan.james.hunt@outlook.com>
2024-07-31 21:27:57 +0100

    added permisions for the eks nodes to reserve ebs space

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -1,3 +1,31 @@
+resource "aws_iam_policy" "ebs_csi_policy" {
+  name        = "EKS_EBS_CSI_Policy"
+  description = "Policy for EKS nodes to manage EBS volumes"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "ec2:CreateSnapshot",
+          "ec2:AttachVolume",
+          "ec2:CreateVolume",
+          "ec2:DeleteSnapshot",
+          "ec2:DeleteVolume",
+          "ec2:DescribeAvailabilityZones",
+          "ec2:DescribeInstances",
+          "ec2:DescribeSnapshots",
+          "ec2:DescribeTags",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeVolumesModifications",
+          "ec2:DetachVolume"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role" "nodes" {
   name = "eks-node-group-nodes"
 
@@ -25,6 +53,11 @@ resource "aws_iam_role_policy_attachment" "nodes-AmazonEKS_CNI_Policy" {
 
 resource "aws_iam_role_policy_attachment" "nodes-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.nodes.name
+}
+
+resource "aws_iam_role_policy_attachment" "nodes-EBS_CSI_Policy" {
+  policy_arn = aws_iam_policy.ebs_csi_policy.arn
   role       = aws_iam_role.nodes.name
 }
 
