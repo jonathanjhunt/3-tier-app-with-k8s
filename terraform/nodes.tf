548c8cf7df97d4039769bbe78cf06b2d838f7126
Jon Hunt <jonathan.james.hunt@outlook.com>
2024-08-01 10:08:22 +0100

    updated the node size from t3.small to t3.medium and increased scaling

--- terraform/nodes.tf
+++ terraform/nodes.tf
@@ -72,12 +72,12 @@ resource "aws_eks_node_group" "private-nodes" {
   ]
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.small"]
+  instance_types = ["t3.medium"]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 5
-    min_size     = 0
+    min_size     = 1
   }
 
   update_config {
